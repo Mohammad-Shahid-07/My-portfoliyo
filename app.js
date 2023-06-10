37551f6f8158e628c5d62eca499ab971d85938bb
Mohammad Shahid <112233075+Mohammad-Shahid-07@users.noreply.github.com>
2023-06-10 18:43:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+
+const mrHero = document.querySelector(".section-heading");
+const intro = document.querySelector(".intro");
+const dev = document.querySelector(".dev");
+const neonCircle = document.querySelector(".home-img");
+const socialLeft = document.querySelectorAll(".left");
+const socialRight = document.querySelectorAll(".right");
+
+// const hello = document.querySelector(".hello");
+
+// new simpleParallax(proj, {
+//   orientation: "left",
+//   overflow: true,
+//   scale: 1.2,
+//   delay: 5,
+// });
+new simpleParallax(mrHero, {
+  orientation: "down",
+  overflow: true,
+  scale: 5,
+  delay: 10,
+});
+
+new simpleParallax(intro, {
+  orientation: "left",
+  overflow: true,
+  scale: 3,
+  delay: 5,
+});
+// new simpleParallax(neonCircle, {
+//   orientation: "right",
+//   overflow: true,
+//   scale: 3,
+//   delay: 5,
+// });
+new simpleParallax(dev, {
+  orientation: "right",
+  overflow: true,
+  scale: 5,
+  delay: 5,
+});
+
+// new simpleParallax(socialRight, {
+//   orientation: "right",
+//   overflow: true,
+//   scale: 6,
+//   delay: 5,
+// });
+// new simpleParallax(socialLeft, {
+//   orientation: "left",
+//   overflow: true,
+//   scale: 6,
+//   delay: 5,
+// });
+
+//   new simpleParallax(heading, {
+//     orientation: 'up left ',
+//     scale: 1,
+//     delay: 2
+//   });
+
+const skillBar = document.querySelectorAll(".percent");
+const skillBarLength = document.querySelectorAll(".skill-bar-box");
+const skillPercent = [89, 79, 84, 59, 79, 89, 79, 69];
+
+let output = [0, 0, 0, 0, 0, 0, 0, 0];
+
+for (let i = 0; i < skillPercent.length; i++) {
+  output[i] = 0;
+}
+
+output.forEach((barr, index) => {
+  skillBar[index].innerHTML = barr + "%";
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      const timer = setInterval(() => {
+        skillPercent.forEach((bar, index) => {
+          skillBar[index].innerHTML = output[index] + "%";
+          skillBarLength[
+            index
+          ].style.width = `calc(${output[index]}%* 0.7077868852)`;
+          if (output[index] <= bar) {
+            output[index]++;
+          } else if (output[index] === bar) {
+            clearInterval(timer);
+          }
+        });
+      }, 50);
+    }
+  });
+});
+
+observer.observe(skillBar[0]);
