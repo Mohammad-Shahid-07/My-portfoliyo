dd68ebf83a9e3609721be0259657067e977f5d43
Mohammad Shahid <112233075+Mohammad-Shahid-07@users.noreply.github.com>
2023-06-27 09:30:21 +0530

    removed animations

--- app.js
+++ app.js
@@ -58,55 +58,55 @@ new simpleParallax(dev, {
 //     delay: 2
 //   });
 
-const skillBar = document.querySelectorAll(".percent");
-const skillBarLength = document.querySelectorAll(".skill-bar-box");
-const skillPercent = [89, 79, 84, 59, 79, 89, 79, 69];
+// const skillBar = document.querySelectorAll(".percent");
+// const skillBarLength = document.querySelectorAll(".skill-bar-box");
+// const skillPercent = [89, 79, 84, 59, 79, 89, 79, 69];
 
-let output = [0, 0, 0, 0, 0, 0, 0, 0];
+// let output = [0, 0, 0, 0, 0, 0, 0, 0];
 
-for (let i = 0; i < skillPercent.length; i++) {
-  output[i] = 0;
-}
+// for (let i = 0; i < skillPercent.length; i++) {
+//   output[i] = 0;
+// }
 
-output.forEach((barr, index) => {
-  skillBar[index].innerHTML = barr + "%";
-});
+// output.forEach((barr, index) => {
+//   skillBar[index].innerHTML = barr + "%";
+// });
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      const timer = setInterval(() => {
-        skillPercent.forEach((bar, index) => {
-          skillBar[index].innerHTML = output[index] + "%";
-          if (window.innerWidth <= 500) {
-            skillBarLength[
-              index
-            ].style.width = `calc(${output[index]}%* 0.5585996409335727)`;
-          } 
-          else if (window.innerWidth <= 600) {
-            skillBarLength[
-              index
-            ].style.width = `calc(${output[index]}%* 0.6385996409335727)`;
-          } else {
-            skillBarLength[
-              index
-            ].style.width = `calc(${output[index]}%* 0.7077868852)`;
-          }
-          if (output[index] <= bar) {
-            output[index]++;
-          } else if (output[index] === bar) {
-            clearInterval(timer);
-          }
-        });
-      }, 25);
-    }
-  });
-});
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.intersectionRatio > 0) {
+//       const timer = setInterval(() => {
+//         skillPercent.forEach((bar, index) => {
+//           skillBar[index].innerHTML = output[index] + "%";
+//           if (window.innerWidth <= 500) {
+//             skillBarLength[
+//               index
+//             ].style.width = `calc(${output[index]}%* 0.5585996409335727)`;
+//           } 
+//           else if (window.innerWidth <= 600) {
+//             skillBarLength[
+//               index
+//             ].style.width = `calc(${output[index]}%* 0.6385996409335727)`;
+//           } else {
+//             skillBarLength[
+//               index
+//             ].style.width = `calc(${output[index]}%* 0.7077868852)`;
+//           }
+//           if (output[index] <= bar) {
+//             output[index]++;
+//           } else if (output[index] === bar) {
+//             clearInterval(timer);
+//           }
+//         });
+//       }, 25);
+//     }
+//   });
+// });
 
-observer.observe(skillBar[0]);
+// observer.observe(skillBar[0]);
 
-// preloder
-window.onload = function () {
-  const preloader = document.getElementById("loader");
-  preloader.style.display = "none";
-};
+// // preloder
+// window.onload = function () {
+//   const preloader = document.getElementById("loader");
+//   preloader.style.display = "none";
+// };
